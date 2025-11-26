367a568ff0e5ca77624cbb57e14bb36d173bcf67
Thunge Rammohan <thungerammohan@gmail.com>
2025-11-26 18:13:07 +0530

    9commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
 }
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUD_API_KEY'),
-    'API_SECRET': os.getenv('CLOUD_API_SECRET')
+    'CLOUD_API_KEY': os.getenv('CLOUD_API_KEY'),
+    'CLOUD_API_SECRET': os.getenv('CLOUD_API_SECRET')
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
