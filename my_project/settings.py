d7c9341191c95dcf1f43716080a4cc3f35a2f71f
Thunge Rammohan <thungerammohan@gmail.com>
2025-11-27 14:06:59 +0530

    12commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -50,10 +50,12 @@ INSTALLED_APPS = [
     'products',
     'cloudinary',
     'cloudinary_storage',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -151,3 +153,35 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# -------------------------
+# CORS SETTINGS
+# -------------------------
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",     # React dev
+    "https://your-frontend-name.netlify.app",  # if deploying
+    "https://your-frontend-name.vercel.app",
+]
+
+CORS_ALLOW_CREDENTIALS = True   # required for cookies
+
+CORS_ALLOW_HEADERS = ["*"]
+CORS_ALLOW_METHODS = ["*"]
+
+
+# -------------------------
+# CSRF SETTINGS (important for cookies)
+# -------------------------
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "https://own-prac1-1.onrender.com",
+    "https://your-frontend-name.netlify.app",
+    "https://your-frontend-name.vercel.app",
+]
+
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+
